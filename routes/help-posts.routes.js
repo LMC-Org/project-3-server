77d71e54ad8405eb127d706c0ad561d9fcd35df5
lino zimerman <linozimerman@gmail.com>
2023-12-04 17:09:14 +0100

    route to get data for my profile

--- routes/help-posts.routes.js
+++ routes/help-posts.routes.js
@@ -3,23 +3,41 @@ const router = express.Router();
 const HelpPost = require("../models/HelpPost.model");
 
 router.get("/:helpId", (req, res, next) => {
-
-    const {helpId} = req.params
-
+    const { helpId } = req.params
     HelpPost.findById(helpId)
-    
         .populate("creator")
+        .populate("selectedVolunteer")
+        .populate("volunteers")
         .then((helpPost) => {
-            const {title, volunteers, description,location,category, creator } = helpPost
-            res.send({title, volunteers, description,location,category, creator})
+            const { title, volunteers, description, location, category, creator } = helpPost
+            res.send({ title, volunteers, description, location, category, creator })
         })
-        .catch((err)=>("couldn't find help post", err))
+        .catch((err) => ("couldn't find help post", err))
+});
+
+//gets all help posts that have my id in volunteers or selected volunteers
+router.get("/volunteered/:userId", (req, res, next) => {
+    const userId = req.params.userId;
+    let allHelpPostsIVolunteered;
+
+    //console.log('este es el id',req.params.userId);
+    HelpPost.find({ volunteers: userId })
+        .then((result) => {
+            allHelpPostsIVolunteered= result;
+            //console.log('ESTE ES EL allHelpPostsIVolunteered', allHelpPostsIVolunteered)
+            return HelpPost.find({ selectedVolunteer: userId });
+        })
+        .then((allHelpPostsIWasChosen)=>{
+            //console.log('ESTE ES EL allHelpPostsIWasChosen', allHelpPostsIWasChosen)
+            res.send({ allHelpPostsIVolunteered, allHelpPostsIWasChosen })
+        })
+        .catch((err) => ("couldn't find help post", err))
 });
 
 router.post("/createhelp", (req, res, next) => {
-   
-    const { title, location, description, helpImageUrl, creator, category} = req.body;
-    console.log("reqbody", req.body);
+
+    const { title, location, description, helpImageUrl, creator, category } = req.body;
+    //console.log("reqbody", req.body);
     HelpPost.create({
         title,
         location,
@@ -27,14 +45,14 @@ router.post("/createhelp", (req, res, next) => {
         helpImageUrl,
         creator,
         category,
-        
+
     })
-    .then((createdHelp) => {
-        res.json(createdHelp)
-        console.log(createdHelp);
-        console.log("este es el req",createdHelp);
+        .then((createdHelp) => {
+            res.json(createdHelp)
+            //console.log(createdHelp);
+            //console.log("este es el req", createdHelp);
         })
-        .catch((err)=>(err))
+        .catch((err) => (err))
 });
 
 module.exports = router;
