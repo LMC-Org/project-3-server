41a56a0c29158964370b37693687b4080bfb0af6
claratesmon <claraperezmontes@gmail.com>
2023-12-30 12:24:53 +0100

    origin cors update

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: ['http://localhost:5050', 'https://dainty-granita-d0a1eb.netlify.app'],
+      origin: ['http://localhost:5173', 'https://dainty-granita-d0a1eb.netlify.app'],
       optionsSuccessStatus: 200
 
     })
