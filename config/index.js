3e3a257737bbb4ecea2e09231d4666f2358645c8
claratesmon <claraperezmontes@gmail.com>
2023-12-31 11:14:12 +0100

    cors test

--- config/index.js
+++ config/index.js
@@ -24,8 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: ['http://localhost:5173', 'https://dainty-granita-d0a1eb.netlify.app'],
-      optionsSuccessStatus: 200
+      origin: [FRONTEND_URL],
 
     })
     
