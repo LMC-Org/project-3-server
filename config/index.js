480fd4e43caee2f238d7a3df76c60067f75924c2
claratesmon <claraperezmontes@gmail.com>
2024-01-05 14:07:26 +0100

    cors frontend url deployed

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL],
+      origin: [FRONTEND_URL, "https://dainty-granita-d0a1eb.netlify.app"],
 
     })
     
